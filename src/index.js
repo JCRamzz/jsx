0e8ea7148bda72a5c1c55aa82ac2b2d8deb4f095
jcramz <jcramz1992@gmail.com>
2023-06-02 23:43:59 -0500

    final commit

--- src/index.js
+++ src/index.js
@@ -10,12 +10,7 @@ const root = ReactDOM.createRoot(el);
 
 // 4) Create a component
 function App() {
-  let message = "Bye there!";
-  if (Math.random() > 0.5) {
-    message = "Hello there!";
-  }
-
-  return <h1>{message}</h1>;
+  return <input placeholder="hi there" />;
 }
 
 // 5) Show the component on the screen
